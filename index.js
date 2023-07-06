d419a88d29d49f251bb24ce766f6ae92903eb137
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-07-06 11:42:43 +0530

    Add random Colors to each new book with accompanying random accent

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ Book.prototype.readStatus = function (){
 //Change the read status, also update the node with new status
 Book.prototype.changeRead = function(){
     if(this.read){
-        this.node.style.color = 'var(--text-color)';
-        this.node.style.backgroundColor = 'var(--main-color)';
+        this.node.style.color = `${randomAccentColor(...this.color)}`;
+        this.node.style.backgroundColor = `${colorToString  (...this.color)}`;
         this.read = false;
     }else{
         this.read = true;
@@ -35,7 +35,7 @@ Book.prototype.changeRead = function(){
         this.node.style.color = 'var(--text-color-dark)';
     }
 
-    this.node.firstChild.querySelector('p:last-child').innerHTML = `<span class="book-label">Status: ${this.readStatus()}`;
+    this.node.firstChild.querySelector('p:last-child').innerHTML = `<span class="book-label">Status: </span>${this.readStatus()}`;
 }
 //Delete a book
 Book.prototype.deleteBook = function (){
@@ -44,33 +44,74 @@ Book.prototype.deleteBook = function (){
     myLibrary.splice(index,1)    
 }
 //Create a new book and add it to the library
-function addBookToLibrary(title, author, pages, read, color){
+function addBookToLibrary(title, author, pages, read){
     //Making object from given arguements
     const container = document.querySelector('.container');
+    const color = randomColor();
+    const accentColor = randomAccentColor(...color);
+    const colorString = colorToString(...color);
     const newBook = new Book(title, author, pages, read, color);
     const bookId = `book${myLibrary.length}`;
     myLibrary.push(newBook);
     //creating the container div for the book
     const div = document.createElement('div');
     div.setAttribute('id', bookId);
-    div.style.borderColor = `${color}`;
+    //styling the container div with random color
+    div.style.borderColor = `${accentColor}`;
+    div.style.backgroundColor = `${colorString}`;
+    div.style.color = `${accentColor}`;
+    div.addEventListener('mouseover', () => {
+        if (newBook.read) {
+            div.style.borderLeft = `0px solid var(--accent-color)`;
+            div.style.boxShadow = `0px 0px 0px 5px var(--accent-color)`;
+        } else {
+            div.style.borderLeft = `0px solid ${accentColor}`;
+            div.style.boxShadow = `0px 0px 0px 5px ${accentColor}`;
+        }
+    });
+    div.addEventListener('mouseout', () => {
+        if (newBook.read) {
+            div.style.borderLeft = `10px solid var(--accent-color)`;
+            div.style.boxShadow = `20px 17px 48px -27px var(--accent-color)`;
+        } else {
+            div.style.borderLeft = `10px solid ${accentColor}`;
+            div.style.boxShadow = `20px 17px 48px -27px ${accentColor}`; 
+        }
+    });
     //creating the div that contains the details about the book
     const infoDiv = document.createElement('div');
     infoDiv.innerHTML = newBook.info();
     div.appendChild(infoDiv);
-    //creating the checkbox for the label
+    //creating the checkbox and label for read status
+    const div2 = document.createElement('div');
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
-    checkbox.addEventListener('change', () => newBook.changeRead());
-    div.appendChild(checkbox);
-    //creating the label to select read status
+    checkbox.addEventListener('change', () => {
+        newBook.changeRead()
+        if (newBook.read) {
+            div.style.boxShadow = `0px 0px 0px 5px var(--accent-color)`;
+        }
+        else {
+            div.style.boxShadow = `0px 0px 0px 5px ${accentColor}`;
+        }
+    });
+    div2.appendChild(checkbox);
     const label = document.createElement('label');
     label.innerHTML = 'Read';
-    div.append(label);
+    div2.appendChild(label);
+    div.appendChild(div2);
     //appending the div to the container
     container.appendChild(div);
     newBook.node = div;
     const delBtn = document.createElement('button');
+    //If the book is read
+    if (newBook.read) {
+        checkbox.setAttribute('checked', 'true');
+        div.style.color = 'var(--text-color-dark)';
+        div.style.backgroundColor = 'rgb(196, 255, 196)';
+        div.style.borderLeft = `10px solid var(--accent-color)`;
+        div.style.boxShadow = `20px 17px 48px -27px var(--accent-color)`;
+    }
     //creating the delete buttonZ
     delBtn.innerText = 'Delete'
     delBtn.addEventListener('click', () => newBook.deleteBook());
@@ -112,3 +153,34 @@ addBookToLibrary('The Lord of the Rings', 'J.R.R. Tolkien', 1178, false, '');
 addBookToLibrary('The Silmarillion', 'J.R.R. Tolkien', 365, false, '');
 addBookToLibrary('The Children of Hurin', 'J.R.R. Tolkien', 313, false, '');
 addBookToLibrary('The Fall of Gondolin', 'J.R.R. Tolkien', 304, false, '');
+
+
+function randomColor() {
+    let h = Math.floor(Math.random() * 360);
+    const s = 100;
+    const l = 88;
+    const sign = Math.floor(Math.random() * 2);
+    if (h > 75 && h < 150) {
+        if (sign == 0) {
+            h = h - 75;
+        }
+        else {
+            h = h + 75;
+        }
+    }
+    return [h,s,l];
+}
+
+function colorToString(h,s,l) {
+    return `hsl(${h}, ${s}%, ${l}%)`;}
+
+
+function randomAccentColor(h,s,l) {
+    h = h + 10;
+    s = s - 58;
+    l = l - 45;
+    if (h > 360) {
+        h = h - 360;
+    }
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
